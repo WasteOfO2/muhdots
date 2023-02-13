be04dd563cdb6cc18a5648dc3f3909c73a2dde81
WasteOfO2 <pvpsocialonline18@mail.com>
2023-02-14 00:32:52 +0530

    initial commit

---
+++ hypr/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+hyprctl keyword windowrule "workspace 2 unset,org.nicotine_plus.Nicotine"
+hyprctl keyword windowrule "workspace 3 unset,io.gitlab.librewolf-community"
+hyprctl keyword windowrule "workspace 1 unset,org.qbittorrent.qBittorent"
